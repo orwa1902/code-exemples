5adcd3d376c5fac1b21715b7fe9430594d3d839c
orwa1902 <omri1362@gmail.com>
2017-10-25 16:30:28 +0300

    Update 21loses.py

--- 21loses.py
+++ 21loses.py
@@ -1,20 +1,20 @@
 from random import randint
-a = 0
-b = 0
+first= 0
+second = 0
 turn = randint(1,2)
 playing = True
 while (playing == True):
   if turn == 1:
-    print("player one turn")
-    a += randint(1,2)
-    print (a)
+    print("first player turn")
+    first += randint(1,2)
+    print (first)
   else:
           print("second player turn")
-    b += randint(1,2)
-    print(b)
+    second += randint(1,2)
+    print(second)
   if a >= 21:
     print("first player lost!")
     break;
   elif b >= 21:
-    print("\n\nsecond player lost!")
+    print("\nsecond player lost!")
     break;
